7902d7c95bedeb5c180db6dd3d108666664a0f26
Bruno Meneses <70494415+brunomenesees@users.noreply.github.com>
2021-05-07 13:53:22 -0300

    Add files via upload

---
+++ itemInfo.lua
@@ -0,0 +1,3 @@
+{}[1] = {unidentifiedDisplayName = "\184\211\184\174\182\236", unidentifiedResourceName = "\180\171\176\161\184\174\176\179", unidentifiedDescriptionName = {}, identifiedDisplayName = "\184\182\183\194\192\199 \186\176", identifiedResourceName = "\184\182\183\194\192\199\186\176", identifiedDescriptionName = {}, slotCount = 0, ClassNum = 0}
+--[[ DECOMPILER ERROR 816: Confused about usage of registers ]]
+
